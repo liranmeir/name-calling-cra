9ed2fca2e1cbe3c6e8af263fb7dcd071eae77a18
liranmeir <liranmeir@gmail.com>
2023-05-18 14:45:00 +0300

    working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import liranImg from "./assets/liran.png";
 import liranAudio from "./assets/liran.mp3";
 import tzippiImg from "./assets/tzippi.png";
@@ -30,7 +30,9 @@ const FAMILY_AUDIO_MAP: FamilyAudioMap = {
 };
 
 function App() {
-  const [multiplier, setMultiplier] = useState(1);
+  const lockRef = useRef<boolean | null>(null);
+  const [enlargedIndex, setEnlargedIndex] = useState<number | null>(null);
+
   const {
     interimTranscript,
     listening,
@@ -42,28 +44,53 @@ function App() {
     SpeechRecognition.startListening({ continuous: true });
   }, []);
 
-  const handleClick = (p: string) => {
+  useEffect(() => {
+    handleInterimTranscript(interimTranscript);
+
+    function handleInterimTranscript(interimTranscript: string) {
+      if (interimTranscript.toLowerCase() === "abba") {
+        // SpeechRecognition.stopListening();
+        handleClick("liran", 0);
+      }
+      
+    }
+  }, [interimTranscript]);
+
+  const handleClick = (p: string, index: number) => {
+    if (lockRef.current) {
+      return;
+    }
+    lockRef.current = true;
     const audio = FAMILY_AUDIO_MAP[p].audio;
-    audio.play();
+    audio.play()
+        .then(() => {
+          setEnlargedIndex(index);
+
+          setTimeout(() => {
+            setEnlargedIndex(null);
+            lockRef.current = null;
+          }, 2000);
+        })
+        .catch((error) => {
+          console.error('Failed to play audio:', error);
+        });
   };
 
   return (
     <div className="App">
       <div>
         <h1>
-          {" "}
           {listening && "you said:"}
           {interimTranscript}
         </h1>
       </div>
       <div className="wrapper">
         {Object.keys(FAMILY_AUDIO_MAP).map((p, index) => {
-          let randomNumber = Math.floor(Math.random() * 4) + 1;
-          const style =
-            randomNumber === index ? { transform: `scale(${multiplier})` } : {};
+          const enlargeClassName = enlargedIndex === index ? "enlarged" : "";
           return (
-            <div className="card" style={style}>
-              <button onClick={() => handleClick(p)}>
+            <div className={"card " + enlargeClassName} key={index}>
+              <button onClick={() => handleClick(p, index)}>
+                {/*  eslint-disable-next-line jsx-a11y/alt-text */}
                 <img src={FAMILY_AUDIO_MAP[p].image} className="card-image" />
               </button>
             </div>
